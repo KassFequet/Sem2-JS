fa78946f1d72b2f649224289707f2846b0fcc6da
KassFequet <kassaundra.fequet@keyin.com>
2025-07-02 09:59:29 -0230

    update

---
+++ Week10/firstapp/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  let myName = "Kass";
+
+  return (
+    <>
+    <h1>Hello world</h1>
+      <p>This is some text</p>
+    </>
+  )
+}
+
+export default App
